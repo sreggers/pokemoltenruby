686dd84175daf36f959a9e1fd5295aa8ac747407
Sebastiaan Reggers <sebastiaan.reggers@posteo.com>
2026-05-28 21:55:13 +0200

    Create nix-shell instructions for building

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # porymap = pkgs.callPackage ./porymap.nix {};
+  python = pkgs.python3.withPackages (
+    python-pkgs: []
+  );
+in
+  pkgs.mkShell {
+    packages = [
+      pkgs.pkgsCross.arm-embedded.stdenv.cc
+      pkgs.git
+      pkgs.pkg-config
+      pkgs.libpng
+      python
+      # porymap
+    ];
+}
